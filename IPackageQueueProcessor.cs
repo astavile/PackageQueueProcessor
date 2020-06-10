1851b83b1970dee84b65d5cb251ae2f213f19aba
astavile <66729739+astavile@users.noreply.github.com>
2020-06-10 14:51:44 +0300

    Update IPackageQueueProcessor.cs

--- IPackageQueueProcessor.cs
+++ IPackageQueueProcessor.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NetProtocol.MessageProcessors
+namespace MessageProcessors
 {
     /// <summary>
     /// Интерфейс для асинхронной обработки очереди пакетов
